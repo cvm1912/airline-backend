72227eb6cc4ec5f1e15ad842988099f74d150ca7
cvm1912 <rajshivam1912@gmail.com>
2026-04-03 10:30:27 +0530

    feature/flight

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes )
-app.listen(ServerConfig.PORT, ()=>{
+app.listen(ServerConfig.PORT, () => {
     console.log(`server is running on http://localhost:${ServerConfig.PORT}`)
     logger.info("successfully started the server")
 })
